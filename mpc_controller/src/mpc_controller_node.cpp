5a8d034050517be10b2bbaba9f547e4208ba4688
Nikolai Flowers <nikolaif399@gmail.com>
2020-09-11 15:51:50 -0400

    Estimator ros skeleton creation (#12)
    * Added more descriptive packages names, adding nodehandle back into classes
    
    * Updated estimator to spin inside class
    
    * Updated controller to spin inside class
    
    * Add Eigen3 as rosdep for controller and estimator
    
    * Removed last trace of yaml-cpp, added custom messages folder
    
    * Added some context to the updateStep function

--- mpc_controller/src/mpc_controller_node.cpp
+++ mpc_controller/src/mpc_controller_node.cpp
@@ -1,5 +1,4 @@
 #include <ros/ros.h>
-#include <yaml-cpp/yaml.h>
 #include <iostream>
 
 #include "mpc_controller/mpc_controller.h"
@@ -8,18 +7,8 @@ int main(int argc, char** argv) {
 	ros::init(argc, argv, "controller_node");
 	ros::NodeHandle nh;
 
-	// Pull needed parameters off rosparam server
-	double update_rate;
-	nh.param<double>("mpc_controller/update_rate", update_rate, 100); // Default to 100 hz
-
-	// Primary logic
 	MPCController mpc_controller(nh);
+	mpc_controller.spin();
 
-	// Control loop frequency
-	ros::Rate r(update_rate);
-	while (ros::ok()) {
-		ros::spinOnce();
-		r.sleep();
-	}
 	return 0;
 }
