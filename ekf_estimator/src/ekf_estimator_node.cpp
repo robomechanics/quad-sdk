f205b85ceb56a1fcfa618fd68c097af8ebc42971
Nikolai Flowers <nikolaif399@gmail.com>
2020-09-11 15:51:50 -0400

    Estimator ros skeleton creation (#12)
    * Added more descriptive packages names, adding nodehandle back into classes
    
    * Updated estimator to spin inside class
    
    * Updated controller to spin inside class
    
    * Add Eigen3 as rosdep for controller and estimator
    
    * Removed last trace of yaml-cpp, added custom messages folder
    
    * Added some context to the updateStep function

--- ekf_estimator/src/ekf_estimator_node.cpp
+++ ekf_estimator/src/ekf_estimator_node.cpp
@@ -1,5 +1,4 @@
 #include <ros/ros.h>
-#include <yaml-cpp/yaml.h>
 #include <iostream>
 
 #include "ekf_estimator/ekf_estimator.h"
@@ -8,18 +7,8 @@ int main(int argc, char** argv) {
 	ros::init(argc, argv, "ekf_estimator_node");
 	ros::NodeHandle nh;
 
-	// Pull needed parameters off rosparam server
-	double update_rate;
-	nh.param<double>("ekf_estimator/update_rate", update_rate, 200); // Default to 200
-
-	// Primary logic
 	EKFEstimator ekf_estimator(nh);
+	ekf_estimator.spin();
 
-	// Control loop frequency
-	ros::Rate r(update_rate);
-	while (ros::ok()) {
-		ros::spinOnce();
-		r.sleep();
-	}
 	return 0;
 }
--- mpc_controller/src/mpc_controller.cpp
+++ mpc_controller/src/mpc_controller.cpp
@@ -3,3 +3,11 @@
 MPCController::MPCController(ros::NodeHandle nh) {
 	nh_ = nh;
 }
+
+void MPCController::spin() {
+	ros::Rate r(update_rate_);
+	while (ros::ok()) {
+		ros::spinOnce();
+		r.sleep();
+	}
+}
