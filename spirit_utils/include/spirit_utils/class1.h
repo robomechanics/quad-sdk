a8a25d33e30dd66a644f48662dfc20a5d1f238e3
Joe Norby <jnorby@andrew.cmu.edu>
2020-09-15 20:06:07 -0400

    Add spirit_utils skeleton (#21)
    * Add spirit_utils skeleton
    
    * Updated example class name in spirit utils, removing executable from utils
    
    Co-authored-by: Nikolai Flowers <nikolaif399@gmail.com>

---
+++ spirit_utils/include/spirit_utils/class1.h
@@ -0,0 +1,18 @@
+#ifndef CLASS1_H
+#define CLASS1_H
+
+//! A template class for spirit
+/*!
+   Class1 is a sample library included in spirit utils.
+*/
+class Class1 {
+public:
+	/**
+	 * @brief Constructor for Class1 Class1
+	 * @return Constructed object of type Class1
+	 */
+	Class1();
+
+};
+
+#endif // CLASS1_H
