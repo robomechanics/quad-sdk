91e7da5f967318fa38eae50541f6a654f9388a78
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-16 00:10:02 +0000

    Deploy robomechanics/spirit-software to github.com/robomechanics/spirit-software.git:gh-pages

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['class1',['Class1',['../classClass1.html',1,'']]],
   ['contactdetection',['ContactDetection',['../classContactDetection.html',1,'']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['class1',['Class1',['../classClass1.html#abda591c8c78ad1de4a10f934e99cce2f',1,'Class1']]],
   ['contactdetection',['ContactDetection',['../classContactDetection.html#a4c33a6d499e48dd4048e04f06114ea6d',1,'ContactDetection']]]
 ];
