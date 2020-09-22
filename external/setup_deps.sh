a53b626aa17b55c2072d1e20639835a00897f8c9
Nikolai Flowers <nikolaif399@gmail.com>
2020-09-22 12:02:28 -0400

    Added external folder with libInterpolate, linked libInterpolate against spirit_utils lib

---
+++ external/setup_deps.sh
@@ -0,0 +1,17 @@
+
+# Install latest version of CMake (on top of CMake provided by catkin_make)
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
+sudo apt update
+sudo apt install cmake -y
+
+# Setup and build for libInterpolate
+sudo apt install libeigen3-dev -y
+sudo apt install libboost-dev libboost-all-dev -y
+
+cd libInterpolate
+mkdir build
+cd build
+cmake ..
+make -j4
+sudo make install
