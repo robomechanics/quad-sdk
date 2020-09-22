1973276a5ff99242acdf6488bc5cded5588d9fc3
Nikolai Flowers <nikolaif399@gmail.com>
2020-09-22 14:00:45 -0400

    Converted interp library to use prior version of cmake

--- external/setup_deps.sh
+++ external/setup_deps.sh
@@ -1,9 +1,5 @@
 
 # Install latest version of CMake (on top of CMake provided by catkin_make)
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
-sudo apt update
-sudo apt install cmake -y
 
 # Setup and build for libInterpolate
 sudo apt install libeigen3-dev -y
