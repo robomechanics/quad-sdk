e9367102afd7dd6c04b0365c2774f9f90068c196
Nikolai Flowers <nikolaif399@gmail.com>
2020-09-25 14:33:59 -0400

    Add custom interp class (#32)
    * Specify full path top top tier README
    
    * Added external folder with libInterpolate, linked libInterpolate against spirit_utils lib
    
    * Updated travis to build all dependencies prior to catkin_make
    
    * Travis tweak
    
    * Travis tweak
    
    * Excluding external folder from doxygen, travis tweak to determine why setup.sh is failing
    
    * Travis tweak
    
    * Removing and readding interpolate library
    
    * Readding interp library
    
    * Placing libInterpolate in the correct place :'(
    
    * Removing libInterpolate from old location
    
    * Converted interp library to use prior version of cmake
    
    * Should be final build tweak for libInterp
    
    * Storing IMU and joint encoder messages
    
    * Added FastInterpolator package under spirit_utils

--- spirit_utils/test/test_main.cpp
+++ spirit_utils/test/test_main.cpp
@@ -1,13 +1,6 @@
 #include <ros/ros.h>
 #include <gtest/gtest.h>
 
-#include "spirit_utils/class1.h"
-
-TEST(Class1, testTrue) {
-	Class1 c1;
-	EXPECT_EQ(1 + 1, 2);
-}
-
 int main(int argc, char** argv) {
 	testing::InitGoogleTest(&argc, argv);
 	ros::init(argc, argv, "tester");
--- spirit_utils/test/test_rviz_interface.cpp
+++ spirit_utils/test/test_rviz_interface.cpp
@@ -8,10 +8,3 @@ TEST(RVizInterfaceTest, testTrue) {
 	RVizInterface rviz_interface(nh);
 	EXPECT_EQ(1 + 1, 2);
 }
-
-// int main(int argc, char** argv) {
-// 	testing::InitGoogleTest(&argc, argv);
-// 	ros::init(argc, argv, "tester");
-
-// 	return RUN_ALL_TESTS();
-// }
