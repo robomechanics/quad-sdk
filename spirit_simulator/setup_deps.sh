1286b89cc622632b3289b9368b9b29b28252e144
Joe Norby <jnorby@andrew.cmu.edu>
2020-10-13 09:46:19 -0400

    Changed simulator setup file to be workspace name agnostic

--- spirit_simulator/setup_deps.sh
+++ spirit_simulator/setup_deps.sh
@@ -1,10 +1,5 @@
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
-cat /etc/apt/sources.list.d/gazebo-stable.list
-wget https://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-sudo apt update
-sudo apt install gazebo9 -y
+GAZEBO_MODEL_PATH_UPDATE="export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$(pwd)/spirit_description:$(pwd)/other/sensor_description:$(pwd)/other/objects_description"
 
-GAZEBO_MODEL_PATH_UPDATE='export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:~/catkin_ws/src/spirit-software/spirit_simulator/spirit_description:~/catkin_ws/src/spirit-software/spirit_simulator/other/sensor_description:~/catkin_ws/src/spirit-software/spirit_simulator/other/objects_description'
 if grep "$GAZEBO_MODEL_PATH_UPDATE" ~/.bashrc > /dev/null
 then
    echo "bashrc contains gazebo model path update"
@@ -12,6 +7,14 @@ else
    echo "Adding gazebo model path update to bashrc"
    echo "$GAZEBO_MODEL_PATH_UPDATE" >> ~/.bashrc
 fi
+
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+cat /etc/apt/sources.list.d/gazebo-stable.list
+wget https://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt update
+sudo apt install gazebo9 -y
+
+
 
 sudo apt install ros-melodic-controller-manager -y
 sudo apt install ros-melodic-joint-state-controller -y
