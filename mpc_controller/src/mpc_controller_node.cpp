c49b07d7150a3ac848e3d6ce8062bdeeeb432ad0
Joe Norby <jcnorby@gmail.com>
2020-10-20 13:04:17 -0400

    Fix indentation

--- mpc_controller/src/mpc_controller_node.cpp
+++ mpc_controller/src/mpc_controller_node.cpp
@@ -4,11 +4,11 @@
 #include "mpc_controller/mpc_controller.h"
 
 int main(int argc, char** argv) {
-	ros::init(argc, argv, "controller_node");
-	ros::NodeHandle nh;
+  ros::init(argc, argv, "controller_node");
+  ros::NodeHandle nh;
 
-	MPCController mpc_controller(nh);
-	mpc_controller.spin();
+  MPCController mpc_controller(nh);
+  mpc_controller.spin();
 
-	return 0;
+  return 0;
 }
--- mpc_controller/test/test_mpc_controller.cpp
+++ mpc_controller/test/test_mpc_controller.cpp
@@ -4,14 +4,14 @@
 #include "mpc_controller/mpc_controller.h"
 
 TEST(MPCController, testTrue) {
-	ros::NodeHandle nh;
-	MPCController mpc_controller(nh);
-	EXPECT_EQ(1 + 1, 2);
+  ros::NodeHandle nh;
+  MPCController mpc_controller(nh);
+  EXPECT_EQ(1 + 1, 2);
 }
 
 int main(int argc, char** argv) {
-	testing::InitGoogleTest(&argc, argv);
-	ros::init(argc, argv, "mpc_controller_tester");
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "mpc_controller_tester");
 
-	return RUN_ALL_TESTS();
+  return RUN_ALL_TESTS();
 }
