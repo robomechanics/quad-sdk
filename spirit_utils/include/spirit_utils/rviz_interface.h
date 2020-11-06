f40ac75c2c42b213eaccbf6f77f58e472f1a2799
Joe Norby <jnorby@andrew.cmu.edu>
2020-11-06 14:33:02 -0500

    Add online visualization of spirit based on state estimate topic (#49)
    * Add robot_interface class to spirit_utils for testing node interfacing
    
    * Actually publish messages in robot_interface
    
    * Completed MPC controller shell. Added ControlInput msg
    
    * Added state estimator puzzle piece
    
    * Fix indentation
    
    * Moved spirit.launch inside spirit_utils
    
    * Fix ControlInput.msg documentation
    
    * Switch state estimate message to represent body odometry with an Odometry message
    
    * State estimator echos dummy joint state messages from robot interface, updated header on state estimate and joint state messages
    
    * Add real-time visualization of robot model given state estimation output
    
    * Move spirit meshes urdf into spirit_description and update launch file to point to it
    
    * Update sensor data passing and urdf to work on the robot
    
    Co-authored-by: Joe Norby <jcnorby@gmail.com>
    Co-authored-by: kongx231 <kongx231@umn.edu>
    Co-authored-by: Nikolai Flowers <nikolaif399@gmail.com>
    Co-authored-by: RML group <robomechanicslab@gmail.com>

--- spirit_utils/include/spirit_utils/rviz_interface.h
+++ spirit_utils/include/spirit_utils/rviz_interface.h
@@ -5,6 +5,9 @@
 #include <spirit_msgs/BodyPlan.h>
 #include <spirit_msgs/Footstep.h>
 #include <spirit_msgs/FootstepPlan.h>
+#include <spirit_msgs/StateEstimate.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
 
 //! A class for interfacing between RViz and spirit-software topics.
 /*!
@@ -13,62 +16,77 @@
 */
 class RVizInterface {
 public:
-	/**
-	 * @brief Constructor for RVizInterface Class
-	 * @param[in] nh ROS NodeHandle to publish and subscribe from
-	 * @return Constructed object of type RVizInterface
-	 */
-	RVizInterface(ros::NodeHandle nh);
+  /**
+   * @brief Constructor for RVizInterface Class
+   * @param[in] nh ROS NodeHandle to publish and subscribe from
+   * @return Constructed object of type RVizInterface
+   */
+  RVizInterface(ros::NodeHandle nh);
 
-	/**
-	 * @brief Calls ros spinOnce and pubs data at set frequency
-	 */
-	void spin();
+  /**
+   * @brief Calls ros spinOnce and pubs data at set frequency
+   */
+  void spin();
 
 private:
-	/**
-     * @brief Callback function to handle new body plan data
-     * @param[in] Body plan message contining interpolated output of body planner
-     */
-    void bodyPlanCallback(const spirit_msgs::BodyPlan::ConstPtr& msg);
+  /**
+   * @brief Callback function to handle new body plan data
+   * @param[in] Body plan message contining interpolated output of body planner
+   */
+  void bodyPlanCallback(const spirit_msgs::BodyPlan::ConstPtr& msg);
 
-    /**
-     * @brief Callback function to handle new body plan discrete state data
-     * @param[in] Body plan message contining discrete output of body planner
-     */
-    void discreteBodyPlanCallback(const spirit_msgs::BodyPlan::ConstPtr& msg);
+  /**
+   * @brief Callback function to handle new body plan discrete state data
+   * @param[in] Body plan message contining discrete output of body planner
+   */
+  void discreteBodyPlanCallback(const spirit_msgs::BodyPlan::ConstPtr& msg);
 
-    /**
-     * @brief Callback function to handle new footstep plan data
-     * @param[in] Footstep plan message containing output of footstep planner
-     */
-    void footstepPlanCallback(const spirit_msgs::FootstepPlan::ConstPtr& msg);
+  /**
+   * @brief Callback function to handle new footstep plan data
+   * @param[in] Footstep plan message containing output of footstep planner
+   */
+  void footstepPlanCallback(const spirit_msgs::FootstepPlan::ConstPtr& msg);
 
-	/// ROS subscriber for the body plan
-	ros::Subscriber body_plan_sub_;
+  /**
+   * @brief Callback function to handle new state estimate data
+   * @param[in] msg State Estimate message containing output of the state estimator node
+   */
+  void stateEstimateCallback(const spirit_msgs::StateEstimate::ConstPtr& msg);
 
-	/// ROS subscriber for the body plan
-	ros::Subscriber discrete_body_plan_sub_;
+  /// ROS subscriber for the body plan
+  ros::Subscriber body_plan_sub_;
 
-	/// ROS subscriber for the footstep plan
-	ros::Subscriber footstep_plan_sub_;
+  /// ROS subscriber for the body plan
+  ros::Subscriber discrete_body_plan_sub_;
 
-	/// ROS Publisher for the interpolated body plan vizualization
-	ros::Publisher body_plan_viz_pub_;
+  /// ROS subscriber for the footstep plan
+  ros::Subscriber footstep_plan_sub_;
 
-	/// ROS Publisher for the discrete body plan vizualization
-	ros::Publisher discrete_body_plan_viz_pub_;
+  /// ROS Subscriber for the state estimate
+  ros::Subscriber state_estimate_sub_;
 
-	/// ROS Publisher for the footstep plan visualization
-	ros::Publisher footstep_plan_viz_pub_;
+  /// ROS Publisher for the interpolated body plan vizualization
+  ros::Publisher body_plan_viz_pub_;
 
-	/// Nodehandle to pub to and sub from
-	ros::NodeHandle nh_;
+  /// ROS Publisher for the discrete body plan vizualization
+  ros::Publisher discrete_body_plan_viz_pub_;
 
-	/// Update rate for sending and receiving data, unused since pubs are called in callbacks
-	double update_rate_;
+  /// ROS Publisher for the footstep plan visualization
+  ros::Publisher footstep_plan_viz_pub_;
 
-	/// Handle for the map frame
+  /// ROS Publisher for the state estimate visualization
+  ros::Publisher joint_states_viz_pub_;
+
+  /// ROS Transform Broadcaster to publish the transform for the base link
+  tf2_ros::TransformBroadcaster base_tf_br_;
+
+  /// Nodehandle to pub to and sub from
+  ros::NodeHandle nh_;
+
+  /// Update rate for sending and receiving data, unused since pubs are called in callbacks
+  double update_rate_;
+
+  /// Handle for the map frame
     std::string map_frame_;
 };
 
