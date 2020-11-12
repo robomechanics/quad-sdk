119081ea4a1301c5e4c7ac1d05ba8ac326e29541
Nikolai Flowers <nikolaif399@gmail.com>
2020-11-12 16:04:28 -0500

    Starting mblink converter

--- spirit_utils/src/mblink_converter.cpp
+++ spirit_utils/src/mblink_converter.cpp
@@ -27,7 +27,6 @@ bool MbLinkConverter::sendMBlink()
   }
 
 
-
   return true;
 }
 
---
+++ spirit_utils/src/mblink_converter_node.cpp
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+#include <iostream>
+
+#include "mblink_converter/mblink_converter.h"
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "mblink_converter_node");
+  ros::NodeHandle nh;
+
+  MBLinkConverter mblink_converter(nh);
+  mblink_converter.spin();
+
+  return 0;
+}
