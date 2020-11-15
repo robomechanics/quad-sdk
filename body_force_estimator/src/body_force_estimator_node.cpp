32cbc022736ee9ec13ffb631035ad54b9fa9d497
Justin Yim <jkyim@andrew.cmu.edu>
2020-11-14 19:46:54 -0500

    Fix pull request comments: parameters, spacing style, and message format (array).

--- body_force_estimator/src/body_force_estimator_node.cpp
+++ body_force_estimator/src/body_force_estimator_node.cpp
@@ -2,11 +2,11 @@
 #include "body_force_estimator/body_force_estimator.h"
 
 int main(int argc, char** argv) {
-	ros::init(argc, argv, "body_force_estimator_node");
-	ros::NodeHandle nh;
+  ros::init(argc, argv, "body_force_estimator_node");
+  ros::NodeHandle nh;
 
-	BodyForceEstimator body_force_estimator(nh);
-	body_force_estimator.spin();
+  BodyForceEstimator body_force_estimator(nh);
+  body_force_estimator.spin();
 
-	return 0;
+  return 0;
 }
--- body_force_estimator/test/test_body_force_estimator.cpp
+++ body_force_estimator/test/test_body_force_estimator.cpp
@@ -4,14 +4,14 @@
 #include "body_force_estimator/body_force_estimator.h"
 
 TEST(BodyContactEstimator, testTrue) {
-	ros::NodeHandle nh;
-	BodyContactEstimator body_force_estimator(nh);
-	EXPECT_EQ(1 + 1, 2);
+  ros::NodeHandle nh;
+  BodyContactEstimator body_force_estimator(nh);
+  EXPECT_EQ(1 + 1, 2);
 }
 
 int main(int argc, char** argv) {
-	testing::InitGoogleTest(&argc, argv);
-	ros::init(argc, argv, "body_force_estimator_tester");
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "body_force_estimator_tester");
 
-	return RUN_ALL_TESTS();
+  return RUN_ALL_TESTS();
 }
