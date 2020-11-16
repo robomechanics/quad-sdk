d887419024bbff952ab6b0a1d68912bb54ce188e
Joe Norby <jnorby@andrew.cmu.edu>
2020-11-16 14:37:04 -0500

    Separate launch files into robot driver and offboard, add GRF info to trajectory (#60)
    * Update the footstep planner to include desired gait sequence, timing, and body velocity dependence
    
    * Add FastTerrainMap and implement within global_body_planner
    
    * Add FastTerrainMap and terrain dependence to local_footstep_planner
    
    * Add test for FastTerrainMap
    
    * Update visualization to surface not point cloud
    
    * Add basic image terrain publishing
    
    * Add image processing utility to terrain_map_publisher
    
    * Update yaml and launch files
    
    * Remove outdated ImageToGridmapDemo
    
    * Remove outdated launch and yaml files
    
    * Reorganize global_body_planner, fix slowdown, add diagnostics capabilities
    
    * Temporarily removing arm build
    
    * Removed all extraneous tests from libInterpolate
    
    * Restoring ARM build to Travis, cleaning up after Joe's beautfil software engineering skills :)
    
    * Rename 'functions' to 'planning_utils' and add namespace protection
    
    * Clean up code a bit, switch to const variable declarations rather than #defines
    
    * Add robot_interface class to spirit_utils for testing node interfacing
    
    * Actually publish messages in robot_interface
    
    * Completed MPC controller shell. Added ControlInput msg
    
    * Added state estimator puzzle piece
    
    * Fix indentation
    
    * Moved spirit.launch inside spirit_utils
    
    * Fix ControlInput.msg documentation
    
    * Switch state estimate message to represent body odometry with an Odometry message
    
    * State estimator echos dummy joint state messages from robot interface, updated header on state estimate and joint state messages
    
    * Add real-time visualization of robot model given state estimation output
    
    * Add Doxygen comments and more rosparams to global_body_planner
    
    * Start updating gbp to take in start and goal states over topics/params
    
    * Move start and goal state to rosparams
    
    * Move spirit meshes urdf into spirit_description and update launch file to point to it
    
    * Update sensor data passing and urdf to work on the robot
    
    * Update .travis.yml to point to main instead of master
    
    * testing pull request
    
    * Force ekf_estimator and robot_interface to default to unit quaternions rather than zeros
    
    * Allow gbp to continually publish better paths, strip down to just rrt_connect (removing rrt and rrt_start_connect), simplify statistics reporting
    
    * Only print statistics if a better path was found
    
    * Add terrain map projection
    
    * Add wrench data to body plan message and visualize in RViz
    
    * body force estimator creation.
    
    * Body force estimator package skeleton.
    
    * Fix pull request comments: parameters, spacing style, and message format (array).
    
    * package.xml was missing spirit_utils
    
    * test_body_force_estimator should build now.
    
    * Add robot_driver.launch
    
    * Update launch files and params to maintain functionality with robot
    
    * Clean up spirit.launch
    
    Co-authored-by: Joe Norby <jcnorby@gmail.com>
    Co-authored-by: Nikolai Flowers <nikolaif399@gmail.com>
    Co-authored-by: kongx231 <kongx231@umn.edu>
    Co-authored-by: RML group <robomechanicslab@gmail.com>
    Co-authored-by: Kongx231 <njkong@andrew.cmu.edu>
    Co-authored-by: Justin Yim <jkyim@andrew.cmu.edu>
    Co-authored-by: justinyim <justin.ks.yim@gmail.com>

--- local_footstep_planner/src/local_footstep_planner.cpp
+++ local_footstep_planner/src/local_footstep_planner.cpp
@@ -1,5 +1,4 @@
 #include "local_footstep_planner/local_footstep_planner.h"
-#include <libInterpolate/Interpolate.hpp>
 #include <tf/tf.h>
 #include <chrono>
 
@@ -52,7 +51,7 @@ void LocalFootstepPlanner::bodyPlanCallback(const spirit_msgs::BodyPlan::ConstPt
     t_plan_.push_back(t_plan.toSec());
 
     // Get the state associated with this data
-    BodyState s;
+    std::vector<double> s(9);
     s[0] = msg->states[i].pose.pose.position.x;
     s[1] = msg->states[i].pose.pose.position.y;
     s[2] = msg->states[i].pose.pose.position.z;
@@ -65,6 +64,45 @@ void LocalFootstepPlanner::bodyPlanCallback(const spirit_msgs::BodyPlan::ConstPt
     body_plan_.push_back(s);
   }
 }
+
+std::vector<double> LocalFootstepPlanner::interpolateMat(std::vector<double> input_vec, std::vector<std::vector<double>> input_mat, double query_point) {
+
+  // Check bounds, throw an error if invalid since this shouldn't ever happen
+  if ((query_point < input_vec.front()) || (query_point > input_vec.back())){
+    throw std::runtime_error("Tried to interpolate out of bounds");
+  }
+
+  // Declare variables for interpolating between, both for input and output data
+  double t1, t2;
+  std::vector<double> y1, y2, interp_data;
+
+  // Find the correct values to interpolate between
+  int idx=0;
+  for(int i=0;i<input_vec.size();i++)
+  {
+      if(input_vec[i]<=query_point && query_point<input_vec[i+1])
+      {
+        t1 = input_vec[i];
+        t2 = input_vec[i+1];
+        y1 = input_mat[i];
+        y2 = input_mat[i+1]; 
+        break;
+      }
+  }
+
+  // Apply linear interpolation for each element in the vector
+  for (int i = 0; i<input_mat.front().size(); i++) {
+    double result = y1[i] + (y2[i]-y1[i])/(t2-t1)*(query_point-t1);
+    interp_data.push_back(result);
+  }
+  
+  return interp_data;
+}
+
+double LocalFootstepPlanner::interpolateVec(std::vector<double> input_vec, std::vector<double> input_mat, double query_point){
+
+}
+
 
 void LocalFootstepPlanner::updatePlan() {
 
@@ -82,28 +120,6 @@ void LocalFootstepPlanner::updatePlan() {
   double x_offsets[4] = {0.3, -0.3, 0.3, -0.3};
   double y_offsets[4] = {0.2, 0.2, -0.2, -0.2};
 
-  // Create transposed body plan for easy interpolation
-  std::vector<std::vector<double>> body_plan_transpose;
-  for (int i=0; i<9; i++) {
-    std::vector<double> v;
-    for (auto s : body_plan_) {
-      v.push_back(s[i]);
-    }
-    body_plan_transpose.push_back(v);
-  }
-
-  // Define interpolators for each variable (TODO: condense this)
-  _1D::LinearInterpolator<double> interp_x;
-  _1D::LinearInterpolator<double> interp_y;
-  _1D::LinearInterpolator<double> interp_dx;
-  _1D::LinearInterpolator<double> interp_dy;
-  _1D::LinearInterpolator<double> interp_yaw;
-  interp_x.setData(t_plan_,body_plan_transpose[0]);
-  interp_y.setData(t_plan_,body_plan_transpose[1]);
-  interp_dx.setData(t_plan_,body_plan_transpose[3]);
-  interp_dy.setData(t_plan_,body_plan_transpose[4]);
-  interp_yaw.setData(t_plan_,body_plan_transpose[8]);
-
   // Loop through each gait cycle
   for (int i = 0; i < num_cycles; i++) {
     
@@ -112,26 +128,28 @@ void LocalFootstepPlanner::updatePlan() {
 
     // Loop through each foot
     for (int j=0; j<num_feet; j++) {
-      FootstepState footstep;
+      FootstepState footstep(5);
 
       // Compute the touchdown and midstance times
       double t_touchdown = t_cycle + t_offsets[j];
       double t_midstance = t_cycle + t_offsets[j] + 0.5*t_s[j];
 
+      BodyState s_touchdown = interpolateMat(t_plan_, body_plan_, t_touchdown);
+      BodyState s_midstance = interpolateMat(t_plan_, body_plan_, t_midstance);
+
       // Compute the body and hip positions and velocities
-      double x_body = interp_x(t_touchdown);
-      double y_body = interp_y(t_touchdown);
-      double x_hip = x_body + x_offsets[j]*cos(interp_yaw(t_touchdown)) 
-        - y_offsets[j]*sin(interp_yaw(t_touchdown));
-      double y_hip = y_body + x_offsets[j]*sin(interp_yaw(t_touchdown)) 
-        + y_offsets[j]*cos(interp_yaw(t_touchdown));
-      double dx_body = interp_x(t_midstance);
-      double dy_body = interp_x(t_midstance);
+      double x_body = s_touchdown[0];
+      double y_body = s_touchdown[1];
+      double yaw = s_touchdown[8];
+      double x_hip = x_body + x_offsets[j]*cos(yaw) - y_offsets[j]*sin(yaw);
+      double y_hip = y_body + x_offsets[j]*sin(yaw) + y_offsets[j]*cos(yaw);
+      double dx_body = s_midstance[3];
+      double dy_body = s_midstance[4];
 
       // Load the data into the footstep array and push into the plan
       footstep[0] = j;
-      footstep[1] = x_hip + 0.5*t_s[j]*interp_dx(t_midstance);
-      footstep[2] = y_hip + 0.5*t_s[j]*interp_dy(t_midstance);
+      footstep[1] = x_hip + 0.5*t_s[j]*dx_body;
+      footstep[2] = y_hip + 0.5*t_s[j]*dy_body;
       footstep[3] = t_touchdown;
       footstep[4] = t_s[j];
 
