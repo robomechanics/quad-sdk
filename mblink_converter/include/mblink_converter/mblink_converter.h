72a88bd451b63d2f0560c56df3d3fc4c4c380950
Nikolai Flowers <nikolaif399@gmail.com>
2020-11-18 11:18:04 -0800

    Nf mblink2 (#63)
    * Specify full path top top tier README
    
    * Added external folder with libInterpolate, linked libInterpolate against spirit_utils lib
    
    * Updated travis to build all dependencies prior to catkin_make
    
    * Travis tweak
    
    * Travis tweak
    
    * Excluding external folder from doxygen, travis tweak to determine why setup.sh is failing
    
    * Travis tweak
    
    * Removing and readding interpolate library
    
    * Readding interp library
    
    * Placing libInterpolate in the correct place :'(
    
    * Removing libInterpolate from old location
    
    * Converted interp library to use prior version of cmake
    
    * Should be final build tweak for libInterp
    
    * Storing IMU and joint encoder messages
    
    * Update progress
    
    * Added include guard to ros_utils
    
    * Added mblink converter in estimator for now
    
    * Starting mblink converter
    
    * Added SpiritSDK (needed for mblink converter), linked mblink against converter node
    
    * Intermediate progress
    
    * Temporarily removing spirit sdk
    
    * Added SpiritSDK back in
    
    * Fixed some controller plugin bugs
    
    * Reset simulation tstep to ghost recommendation, clamping gazebo control plugin to realizable torques
    
    * Updated MBLink converter to listen to new rmessage types for control
    
    * Make sure spirit_msgs gets built before gazebo contact plugin
    
    * Adding CMake flag to determine system architecture and include the correct grmblink binaries
    
    * Modified arch name again
    
    * Just ignore this package on arm

---
+++ mblink_converter/include/mblink_converter/mblink_converter.h
@@ -0,0 +1,79 @@
+#ifndef MBLINK_CONVERTER_H
+#define MBLINK_CONVERTER_H
+
+#include <ros/ros.h>
+#include <spirit_msgs/LegCommand.h>
+#include <spirit_msgs/LegCommandArray.h>
+#include <mblink/mblink.hpp>
+#include <eigen3/Eigen/Eigen>
+#include <chrono>
+#include <thread>
+
+using gr::MBLink;
+
+struct LimbCmd_t {
+  float pos[3];
+  float vel[3];
+  float tau[3];
+  short kp[3];
+  float kd[3];
+};
+
+//! Implements online conversion from ROS type to MBLink
+/*!
+   MBLinkConverter listens for joint control messages and outputs low level commands to the mainboard over mblink
+*/
+class MBLinkConverter{
+public:
+  /**
+   * @brief Constructor for MBLinkConverter
+   * @param[in] nh ROS NodeHandle to publish and subscribe from
+   * @param[in] mblink Pointer to MBLink object
+   * @return Constructed object of type EKFEstimator
+   */
+  MBLinkConverter(ros::NodeHandle nh, std::shared_ptr<MBLink> mblink);
+
+  /**
+   * @brief Calls ros spinOnce and spins at set frequency
+   */
+  void spin();
+
+private:
+  /**
+   * @brief Callback function to handle new leg command data
+   * @param[in] msg spirit_msgs<LegCommandArray> containing pos, vel and torque setpoints and gains
+   */
+  void legControlCallback(const spirit_msgs::LegCommandArray::ConstPtr& msg);
+
+  /**
+   * @brief Compress two floats into one
+   * @param[in] in1 First float to be packed
+   * @param[out] in2 Second float to be packed
+   * @return Compressed float
+   */
+  float packFloats(float in1, float in2);
+
+  /**
+   * @brief Send most recent motor command over mblink
+   * @return Boolean signaling successful mblink send
+   */
+  bool sendMBlink();
+
+  /// Subscriber for motor control messages
+  ros::Subscriber leg_control_sub_;
+
+  /// Nodehandle to pub to and sub from
+  ros::NodeHandle nh_;
+
+  /// Update rate for sending and receiving data;
+  double update_rate_;
+
+  /// Last motor control message (keep sending until we get a new message in or node is shutdown)
+  spirit_msgs::LegCommandArray::ConstPtr last_leg_command_array_msg_;
+
+  /// Pointer to MBLink object (constructor wants argc and argv, so instantiated in main)
+  std::shared_ptr<MBLink> mblink_;
+
+};
+
+#endif
