389aea6899d1ebdbf40cd89e5af78a8a191e8016
Joe Norby <jcnorby@gmail.com>
2020-12-02 20:28:52 -0500

    Update processLog to run from -nodesktop, move recording to robot_driver.launch for now

--- spirit_logger/scripts/processLog.m
+++ spirit_logger/scripts/processLog.m
@@ -1,15 +1,24 @@
-% Open a bag files
-close all;clear;clc;
+function processLog(varargin)
+% Process a bagfile and save the resulting images
+close all;clc;
+
+% If a trial name is provided, use that to save everything
+if nargin>0
+    % Leave these as is so the default is to animate and save
+    trialName = varargin{1};
+    bAnimate = true;
+    bSave = true;
+else
+    % Specify the trial name and settings
+    trialName = 'spirit_framework_test_2020-11-23';
+    bAnimate = true;
+    bSave = true;
+end
 
 % Check that this is the right current directory otherwise paths won't work
 if ~endsWith(pwd, 'spirit-software/spirit_logger/scripts')
     error('This script must be run from spirit-software/spirit_logger/scripts/');
 end
-
-% Specify the trial name and settings
-trialName = 'spirit_framework_test_2020-11-23';
-bAnimate = true;
-bSave = true;
 
 % Import URDF
 figure
@@ -33,7 +42,7 @@ stateGroundTruth = data.stateGroundTruth;
 % Save the data if desired
 logDir = [];
 if bSave
-    logDir = saveLog(bSave, trialName, COMTrajFig, linearStateFig, angularStateFig, jointPositionFig, jointVelocityFig, jointEffortFig);
+    logDir = saveLog(trialName, COMTrajFig, linearStateFig, angularStateFig, jointPositionFig, jointVelocityFig, jointEffortFig);
 end
 
 % Animate and save if desired
