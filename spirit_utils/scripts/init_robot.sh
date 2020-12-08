9db705d663e1ba8a2dcd55ee72a987d3c3b7d252
Joe Norby <jnorby@andrew.cmu.edu>
2020-12-08 13:36:17 -0500

    Add MATLAB parsing and clean up driver launch files (#75)
    * Add more functional parsing scripts
    
    * Update processLog to run from -nodesktop, move recording to robot_driver.launch for now
    
    * Streamline launchfiles, rename record.launch to logging.launch, remove section from init_robot.sh that calls robot_driver.launch
    
    * Move ground_truth_publisher back to mocap.launch
    
    Co-authored-by: Joe Norby <jcnorby@gmail.com>

--- spirit_utils/scripts/init_robot.sh
+++ spirit_utils/scripts/init_robot.sh
@@ -4,14 +4,14 @@ source /home/ghost/catkin_ws/devel/setup.bash
 echo "Setting ROS_MASTER_URI and ROS_IP to 192.168.168.105"
 export ROS_MASTER_URI=http://192.168.168.105:11311
 export ROS_IP=192.168.168.105
-WITH_MOCAP="false"
-WITH_PROXY="false"
-while getopts "mi" flag
-do
-  case $flag in
-    m) WITH_MOCAP="true"; echo "using mocap" ;;
-    i) WITH_PROXY="true"; echo "using proxy robot" ;;
-  esac
-done
-echo "Launching robot_driver"
-roslaunch spirit_utils robot_driver.launch with_mocap:=$WITH_MOCAP with_proxy:=$WITH_PROXY
+# MOCAP="false"
+# PROXY="false"
+# while getopts "mp" flag
+# do
+#   case $flag in
+#     m) MOCAP="true"; echo "using mocap" ;;
+#     p) PROXY="true"; echo "using proxy robot" ;;
+#   esac
+# done
+# echo "Launching robot_driver"
+# roslaunch spirit_utils robot_driver.launch mocap:=$MOCAP proxy:=$PROXY
