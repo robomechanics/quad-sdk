e5fc3c3a98c41c77a305c25e3463a440132c9819
jcnorby <jnorby@andrew.cmu.edu>
2021-02-26 16:23:32 -0500

    Update test_inverse_dynamics.cpp

--- inverse_dynamics/test/test_inverse_dynamics.cpp
+++ inverse_dynamics/test/test_inverse_dynamics.cpp
@@ -1,17 +1,17 @@
 #include <ros/ros.h>
 #include <gtest/gtest.h>
 
-#include "mpc_controller/mpc_controller.h"
+#include "inverse_dynamics/inverse_dynamics.h"
 
-TEST(MPCController, testTrue) {
+TEST(InverseDynamicsTest, testTrue) {
   ros::NodeHandle nh;
-  MPCController mpc_controller(nh);
+  InverseDynamics inverse_dynamics(nh);
   EXPECT_EQ(1 + 1, 2);
 }
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "mpc_controller_tester");
+  ros::init(argc, argv, "inverse_dynamics_test");
 
   return RUN_ALL_TESTS();
 }
