4391ed57d570330334146e55ae36f899b251c79a
Justin Yim <jkyim@andrew.cmu.edu>
2021-07-21 09:06:01 -0400

    Adding orientation and foot force tests to test_kinematics.

--- spirit_utils/test/test_kinematics.cpp
+++ spirit_utils/test/test_kinematics.cpp
@@ -13,19 +13,19 @@ TEST(KinematicsTest, testDifferentialFKIK)
   // Declare kinematics object
   SpiritKinematics kinematics;
 
-  for (size_t i = 0; i < 10; i++)
+  for (size_t i = 0; i < 20; i++)
   {
     // Declare input and output RobotState object
     spirit_msgs::RobotState state, state_out;
 
     // Random velocities at origin
     Eigen::VectorXd body_state(12);
-    body_state << 0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
+    body_state << (double)rand() / RAND_MAX - 0.5,
+        (double)rand() / RAND_MAX - 0.5,
+        (double)rand() / RAND_MAX - 0.5,
+        1.5 * (double)rand() / RAND_MAX - 0.75,
+        1.5 * (double)rand() / RAND_MAX - 0.75,
+        1.5 * (double)rand() / RAND_MAX - 0.75,
         10 * (double)rand() / RAND_MAX - 5,
         10 * (double)rand() / RAND_MAX - 5,
         10 * (double)rand() / RAND_MAX - 5,
@@ -71,6 +71,154 @@ TEST(KinematicsTest, testDifferentialFKIK)
     Eigen::VectorXd error = vel - vel_out;
     EXPECT_TRUE(error.norm() <= kinematics_tol);
   }
+}
+
+
+TEST(KinematicsTest, testFootForces) {
+
+  // Length parameters from URDF
+  // TODO: load these from parameters rather than hard-coding
+  Eigen::MatrixXd ls(4,3);
+  ls << 0.2263, 0.07, 0.0, // abad from body
+    0.0, 0.10098, 0.0, // hip from abad
+    -0.206, 0.0, 0.0, // knee from hip
+    0.206, 0.0, 0.0; // toe from knee
+
+  double pi = 3.14159265359;
+    
+  // Define vectors for states, forces, and torques
+  Eigen::VectorXd state_positions(18), forces(12), torques(18), torques_solution(18);
+
+  // Compute jacobian
+  Eigen::MatrixXd jacobian = Eigen::MatrixXd::Zero(12, 18);
+  
+  // Set up known solution problem 1 ----------------------------------
+  state_positions = Eigen::VectorXd::Zero(18);
+  for (int i=0; i<3; i++){
+    // move the CG around randomly -- it should not matter
+    state_positions(12+i) = (double)rand()/RAND_MAX - 0.5;
+  }
+  forces = Eigen::VectorXd::Zero(12);
+  forces(2) = 3.0; // front left toe Z
+  forces(3) = 2.0; // back left toe X
+
+  // Known solution
+  torques_solution << 3.0 * ls(1,1),
+    0.0,
+    3.0 * -ls(3,0),
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    2.0,
+    0.0,
+    3.0,
+    3.0 * (ls(0,1) + ls(1,1)),
+    3.0 * -ls(0,0),
+    -2.0 * (ls(0,1) + ls(1,1));
+
+  // Compute joint torques
+  spirit_utils::getJacobian(state_positions, jacobian);
+  torques = jacobian.transpose() * forces;
+
+  // Check the answers
+  Eigen::VectorXd error = torques - torques_solution;
+  Eigen::MatrixXd toPrint(18,2);
+  toPrint << torques, torques_solution;
+  std::cout << "Test 1:\n" << toPrint << std::endl
+  << torques_solution.transpose() << std::endl;
+  EXPECT_TRUE(error.norm() <= kinematics_tol);
+
+  // Set up known solution problem 2 ----------------------------------
+  state_positions = Eigen::VectorXd::Zero(18);
+  for (int i=0; i<3; i++){
+    // move the CG around randomly -- it should not matter
+    state_positions(12+i) = (double)rand()/RAND_MAX - 0.5;
+  }
+  state_positions(17) = pi/2; // yaw 90 deg left
+  state_positions(7) = pi/4; // front right hip 45 deg down
+  state_positions(8) = pi/2; // front right knee 90 deg down
+  forces = Eigen::VectorXd::Zero(12);
+  forces(6) = 3.0; // front right toe X
+  forces(8) = 5.0; // front right toe Z
+
+  // Known solution
+  torques_solution << 0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    -5.0 * ls(1,1) - 3.0 * (-ls(2,0)*sin(pi/4) + ls(3,0)*sin(pi/4)),
+    0.0,
+    5.0 * -ls(3,0)*cos(pi/4),
+    0.0,
+    0.0,
+    0.0,
+    3.0,
+    0.0,
+    5.0,
+    -5.0 * (ls(0,1) + ls(1,1)) - 3.0 * (-ls(2,0)*sin(pi/4) + ls(3,0)*sin(pi/4)),
+    -5.0 * ls(0,0),
+    -3.0 * ls(0,0);
+
+  // Compute joint torques
+  spirit_utils::getJacobian(state_positions, jacobian);
+  torques = jacobian.transpose() * forces;
+
+  // Check the answers
+  error = torques - torques_solution;
+  toPrint << torques, torques_solution;
+  std::cout << "Test 2:\n" << toPrint << std::endl;
+  EXPECT_TRUE(error.norm() <= kinematics_tol);
+
+  // Set up known solution problem 3 ----------------------------------
+  state_positions = Eigen::VectorXd::Zero(18);
+  for (int i=0; i<3; i++){
+    // move the CG around randomly -- it should not matter
+    state_positions(12+i) = (double)rand()/RAND_MAX - 0.5;
+  }
+  state_positions(15) = pi/2; // roll 90 deg right
+  state_positions(17) = pi/2; // yaw 90 deg left
+  forces = Eigen::VectorXd::Zero(12);
+  forces(0) = 1.0; // front left toe X
+
+  // Known solution
+  torques_solution << ls(1,1),
+    0.0,
+    -ls(3,0),
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    ls(0,1) + ls(1,1),
+    0.0,
+    -ls(0,0);
+
+  // Compute joint torques
+  spirit_utils::getJacobian(state_positions, jacobian);
+  torques = jacobian.transpose() * forces;
+
+  // Check the answers
+  error = torques - torques_solution;
+  toPrint << torques, torques_solution;
+  std::cout << "Test 2:\n" << toPrint << std::endl;
+  EXPECT_TRUE(error.norm() <= kinematics_tol);
+
+
 }
 
 TEST(KinematicsTest, testFKIKFeasibleConfigurations) {
