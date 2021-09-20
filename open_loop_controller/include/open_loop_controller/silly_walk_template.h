d863918e4f3ea1cc085b2818d5679ef1bdcc4378
jcnorby <jnorby@andrew.cmu.edu>
2021-09-20 19:44:28 -0400

    Update for better directions

--- open_loop_controller/include/open_loop_controller/silly_walk_template.h
+++ open_loop_controller/include/open_loop_controller/silly_walk_template.h
@@ -10,16 +10,16 @@
 
 //! A Silly Walk class. Brief class description goes here.
 /*!
-   SillyWalkController implements a silly walk. Put a detailed description of your class here.
+   SillyWalkTemplate implements a silly walk. Put a detailed description of your class here.
 */
-class SillyWalkController {
+class SillyWalkTemplate {
   public:
 	/**
-	 * @brief Constructor for SillyWalkController
+	 * @brief Constructor for SillyWalkTemplate
 	 * @param[in] nh ROS NodeHandle to publish and subscribe from
-	 * @return Constructed object of type SillyWalkController
+	 * @return Constructed object of type SillyWalkTemplate
 	 */
-	SillyWalkController(ros::NodeHandle nh);
+	SillyWalkTemplate(ros::NodeHandle nh);
 
 	/**
 	 * @brief Calls ros spinOnce and pubs data at set frequency
@@ -42,6 +42,14 @@ private:
 	 * @brief Send open loop joint control
 	 */
   void publishJointControl();
+
+	/**
+	 * @brief Send open loop joint control
+	 * @param[in] input Describe variables going into the function (past by const ref if large)
+	 * @param[out] output Describe variables coming out of the function
+	 * @return Describe the return value of the function
+	 */
+  void doxygenExampleFunction(const std::vector<int> &input, std::vector<double> &output);
 
 	/// Nodehandle to pub to and sub from
 	ros::NodeHandle nh_;
@@ -69,6 +77,12 @@ private:
 
 	/// Number of legs
 	const int num_legs_ = 4;
+
+	/// Standing joint angles
+	std::vector<double> stand_joint_angles_;
+
+	// Use a kinematics object to make kinematics calculations easier
+	spirit_utils::SpiritKinematics kinematics_;
 };
 
 
--- open_loop_controller/src/silly_walk_template.cpp
+++ open_loop_controller/src/silly_walk_template.cpp
@@ -1,7 +1,7 @@
 #include "open_loop_controller/silly_walk_template.h"
 
-SillyWalkController::SillyWalkController(ros::NodeHandle nh) {
-	
+SillyWalkTemplate::SillyWalkTemplate(ros::NodeHandle nh) {
+  
   // Assign the node handle to the class
   nh_ = nh;
 
@@ -10,68 +10,97 @@ SillyWalkController::SillyWalkController(ros::NodeHandle nh) {
   spirit_utils::loadROSParam(nh_,"topics/control/joint_command",joint_command_topic);
   spirit_utils::loadROSParam(nh_,"topics/control/mode",control_mode_topic);
   spirit_utils::loadROSParam(nh_,"silly_walk_template/update_rate",update_rate_);
+  spirit_utils::loadROSParam(nh_,"silly_walk_template/stand_angles",stand_joint_angles_);
   
   // Setup pubs and subs
-	joint_control_pub_ = nh_.advertise<spirit_msgs::LegCommandArray>(joint_command_topic,1);
-  control_mode_sub_ = nh_.subscribe(control_mode_topic,1,&SillyWalkController::controlModeCallback, this);
+  joint_control_pub_ = nh_.advertise<spirit_msgs::LegCommandArray>(joint_command_topic,1);
+  control_mode_sub_ = nh_.subscribe(control_mode_topic,1,&SillyWalkTemplate::controlModeCallback, this);
 
   // Add any other class initialization goes here
   control_mode_ = SIT;
 }
 
-void SillyWalkController::controlModeCallback(const std_msgs::UInt8::ConstPtr& msg) {
+void SillyWalkTemplate::controlModeCallback(const std_msgs::UInt8::ConstPtr& msg) {
   
-  // Use this to set any logic for control modes
+  // Use this to set any logic for control modes (see inverse_dynamics for more examples)
   if (msg->data == SIT || (msg->data == STAND))
   {  
     control_mode_ = msg->data;
   }
 }
 
-void SillyWalkController::computeJointControl()
+void SillyWalkTemplate::computeJointControl()
 {
   // Put your control code here
   control_msg_.leg_commands.clear();
   control_msg_.leg_commands.resize(num_legs_);
-}
 
-void SillyWalkController::publishJointControl()
-{
-  control_msg_.header.stamp = ros::Time::now();
-	joint_control_pub_.publish(control_msg_);
-}
+  // The SpiritKinematics class can help do basic kinematic computations (with type Eigen::VectorXd)
+  // For example: kinematics_.legIK(leg_index, body_pos, body_rpy, foot_pos_world,joint_state);
+  // See inverse_dynamics for more elaborate implementations
 
-void SillyWalkController::sendJointPositions()
-{
-	
-  // Control state machine can go here if you want one
+  // You can use something like this if you want a state machine
+  // (This can be useful to implement basic stop/go functionality)
   switch (control_mode_){
-    case SIT: // option 1
+    case SIT: // sit
     {
-
+      for (int i = 0; i < 4; ++i)
+      {
+        control_msg_.leg_commands.at(i).motor_commands.resize(3);
+        for (int j = 0; j < 3; ++j)
+        {
+          control_msg_.leg_commands.at(i).motor_commands.at(j).pos_setpoint = 0;
+          control_msg_.leg_commands.at(i).motor_commands.at(j).vel_setpoint = 0;
+          control_msg_.leg_commands.at(i).motor_commands.at(j).kp = 5;
+          control_msg_.leg_commands.at(i).motor_commands.at(j).kd = 0.1;
+          control_msg_.leg_commands.at(i).motor_commands.at(j).torque_ff = 0;
+        }
+      }
     }
     break;
-    
-    case STAND: // option 2
+
+    case STAND: // stand
     {
-      
+      for (int i = 0; i < 4; ++i)
+      {
+        control_msg_.leg_commands.at(i).motor_commands.resize(3);
+        for (int j = 0; j < 3; ++j)
+        {
+          control_msg_.leg_commands.at(i).motor_commands.at(j).pos_setpoint = stand_joint_angles_.at(j);
+          control_msg_.leg_commands.at(i).motor_commands.at(j).vel_setpoint = 0;
+          control_msg_.leg_commands.at(i).motor_commands.at(j).kp = 5;
+          control_msg_.leg_commands.at(i).motor_commands.at(j).kd = 0.1;
+          control_msg_.leg_commands.at(i).motor_commands.at(j).torque_ff = 0;
+        }
+      }
     }
     break;
   }
-
-	msg.header.stamp = ros::Time::now();
-	joint_control_pub_.publish(msg);
 }
 
-void SillyWalkController::spin() {
+void SillyWalkTemplate::publishJointControl()
+{
+  // Always need to set the timestamp
+  control_msg_.header.stamp = ros::Time::now();
 
-  // Set update rate
+  // Publish the message
+  joint_control_pub_.publish(control_msg_);
+}
+
+void SillyWalkTemplate::spin() {
+
+  // Set update rate and do any other pre-loop stuff
   ros::Rate r(update_rate_);
 
+  // Enter the main loop
   while (ros::ok()) {
-    this->computeJointControl(elapsed_time);
-    this->sendJointPositions(elapsed_time);
 
+    // Compute and publish the control
+    // Doesn't need to be structured this way but keep spin() succinct
+    this->computeJointControl();
+    this->publishJointControl();
+
+    // Always include this to keep the subscribers up to date and the update rate constant
     ros::spinOnce();
     r.sleep();
   }
