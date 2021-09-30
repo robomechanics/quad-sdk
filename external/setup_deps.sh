1811764ee1bbd3d92d57c9a7cc62e9057356b8b7
jcnorby <jnorby@andrew.cmu.edu>
2021-09-30 19:43:32 -0400

    update circleci config.yml

--- external/setup_deps.sh
+++ external/setup_deps.sh
@@ -30,34 +30,34 @@ make -j8
 sudo make install
 cd ../..
 
-# Setup and build for IPOPT
-cd ipopt
-sudo apt-get install gcc g++ gfortran git patch wget pkg-config liblapack-dev libmetis-dev
-if [ -d "./coinbrew" ] 
-then
-	sudo rm -r ./coinbrew
-fi
-git clone https://www.github.com/coin-or/coinbrew
-cd coinbrew
-chmod u+x coinbrew
-./coinbrew fetch Ipopt@3.14 --no-prompt
-cd ..
-if [ -d "./coinhsl" ] 
-then
-    echo "HSL found." 
-	cp -r ./coinhsl ./coinbrew/ThirdParty/HSL/coinhsl
-else
-    echo "Warning: HSL not found."
-fi
-cd coinbrew
-sudo ./coinbrew build Ipopt --prefix=/usr/local --no-prompt --parallel-jobs=8
-cd ../..
+# # Setup and build for IPOPT
+# cd ipopt
+# sudo apt-get install -y gcc g++ gfortran git patch wget pkg-config liblapack-dev libmetis-dev
+# if [ -d "./coinbrew" ] 
+# then
+# 	sudo rm -r ./coinbrew
+# fi
+# git clone https://www.github.com/coin-or/coinbrew
+# cd coinbrew
+# chmod u+x coinbrew
+# ./coinbrew fetch Ipopt@3.14 --no-prompt
+# cd ..
+# if [ -d "./coinhsl" ] 
+# then
+#     echo "HSL found." 
+# 	cp -r ./coinhsl ./coinbrew/ThirdParty/HSL/coinhsl
+# else
+#     echo "Warning: HSL not found."
+# fi
+# cd coinbrew
+# sudo ./coinbrew build Ipopt --prefix=/usr/local --no-prompt --parallel-jobs=8
+# cd ../..
 
-# Setup and build for rbdl
-cd rbdl-orb
-quiet_mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D RBDL_BUILD_ADDON_URDFREADER=ON ..
-make -j8
-sudo make install
-cd ../..
+# # Setup and build for rbdl
+# cd rbdl-orb
+# quiet_mkdir build
+# cd build
+# cmake -D CMAKE_BUILD_TYPE=Release -D RBDL_BUILD_ADDON_URDFREADER=ON ..
+# make -j8
+# sudo make install
+# cd ../..
