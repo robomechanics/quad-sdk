8f7566ab28155184a546b844a9fbb6e66860f9ab
Joe Norby <jnorby@andrew.cmu.edu>
2021-10-01 11:20:46 -0400

    Readd ipopt

--- external/setup_deps.sh
+++ external/setup_deps.sh
@@ -30,28 +30,28 @@ make -j8
 sudo make install
 cd ../..
 
-# # Setup and build for IPOPT
-# cd ipopt
-# sudo apt-get install -y gcc g++ gfortran git patch wget pkg-config liblapack-dev libmetis-dev
-# if [ -d "./coinbrew" ] 
-# then
-# 	sudo rm -r ./coinbrew
-# fi
-# git clone https://www.github.com/coin-or/coinbrew
-# cd coinbrew
-# chmod u+x coinbrew
-# ./coinbrew fetch Ipopt@3.14 --no-prompt
-# cd ..
-# if [ -d "./coinhsl" ] 
-# then
-#     echo "HSL found." 
-# 	cp -r ./coinhsl ./coinbrew/ThirdParty/HSL/coinhsl
-# else
-#     echo "Warning: HSL not found."
-# fi
-# cd coinbrew
-# sudo ./coinbrew build Ipopt --prefix=/usr/local --no-prompt --parallel-jobs=8
-# cd ../..
+# Setup and build for IPOPT
+cd ipopt
+sudo apt-get install -y gcc g++ gfortran git patch wget pkg-config liblapack-dev libmetis-dev
+if [ -d "./coinbrew" ] 
+then
+	sudo rm -r ./coinbrew
+fi
+git clone https://www.github.com/coin-or/coinbrew
+cd coinbrew
+chmod u+x coinbrew
+./coinbrew fetch Ipopt@3.14 --no-prompt
+cd ..
+if [ -d "./coinhsl" ] 
+then
+    echo "HSL found." 
+	cp -r ./coinhsl ./coinbrew/ThirdParty/HSL/coinhsl
+else
+    echo "Warning: HSL not found."
+fi
+cd coinbrew
+sudo ./coinbrew build Ipopt --prefix=/usr/local --no-prompt --parallel-jobs=8
+cd ../..
 
 # Setup and build for rbdl
 sudo apt install -y ros-melodic-urdf
