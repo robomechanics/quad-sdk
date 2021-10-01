b79af9ba5afd3bb8b424eb01ef6701c534462cfb
jcnorby <jnorby@andrew.cmu.edu>
2021-09-30 20:19:36 -0400

    readd rbdl

--- external/setup_deps.sh
+++ external/setup_deps.sh
@@ -53,11 +53,11 @@ cd ../..
 # sudo ./coinbrew build Ipopt --prefix=/usr/local --no-prompt --parallel-jobs=8
 # cd ../..
 
-# # Setup and build for rbdl
-# cd rbdl-orb
-# quiet_mkdir build
-# cd build
-# cmake -D CMAKE_BUILD_TYPE=Release -D RBDL_BUILD_ADDON_URDFREADER=ON ..
-# make -j8
-# sudo make install
-# cd ../..
+# Setup and build for rbdl
+cd rbdl-orb
+quiet_mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=Release -D RBDL_BUILD_ADDON_URDFREADER=ON ..
+make -j8
+sudo make install
+cd ../..
