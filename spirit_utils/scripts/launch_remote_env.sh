adee7afaddb7b3c0490d64c78646001a76444a77
Robomechanics Lab <robomechanicalab@gmail.com>
2021-10-07 14:25:35 -0400

    Update remote and robot initialization scripts

--- spirit_utils/scripts/launch_remote_env.sh
+++ spirit_utils/scripts/launch_remote_env.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-echo "Navigaing to spirit-software"
-cd ~/catkin_ws/src/spirit-software/
-
-echo "Pull and build most recent code (requires internet connection)? [y/n]"
-read input
-
-if [[ $input == "Y" || $input == "y" ]]; then
-	echo "Pulling most recent code from main"
-	git checkout main
-	git pull
-
-	echo "Running setup script to make sure package deps are up to date"
-	./setup.sh
-
-	echo "Compiling"
-	cd ~/catkin_ws/
-	catkin_make
-	cd ~/catkin_ws/src/spirit-software/
-fi
-
 echo "Make sure you sourced this file (not ./)"
 echo "Sourcing environment and setting IPs"
 source ~/catkin_ws/src/spirit-software/spirit_utils/scripts/init_remote.sh
