51eaa1597a02b808f08bf9a2d0092c943196994d
Joe Norby <jnorby@andrew.cmu.edu>
2021-10-06 20:48:17 -0400

    Update init_remote.sh

--- spirit_utils/scripts/init_remote.sh
+++ spirit_utils/scripts/init_remote.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Sourcing ~/catkin_ws/devel/setup.bash"
 source ~/catkin_ws/devel/setup.bash
-echo "Setting ROS_MASTER_URI to 192.168.168.105 and ROS_IP to 192.168.168.5"
-export ROS_MASTER_URI=http://192.168.168.105:11311
-export ROS_IP=192.168.168.5
+echo "Setting ROS_MASTER_URI to 192.168.8.101 and ROS_IP to 192.168.8.102"
+export ROS_MASTER_URI=http://192.168.8.101:11311
+export ROS_IP=192.168.8.102
