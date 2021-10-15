452d018c0729d80c314b81db1bf1279539e97850
Joe Norby <jcnorby@gmail.com>
2021-10-15 18:34:57 -0400

    Update tests for leg controller and inverse dynamics

--- leg_controller/test/test_inverse_dynamics.cpp
+++ leg_controller/test/test_inverse_dynamics.cpp
@@ -1,17 +1,23 @@
 #include <ros/ros.h>
 #include <gtest/gtest.h>
 
-#include "inverse_dynamics/inverse_dynamics.h"
+#include "leg_controller/inverse_dynamics.h"
+#include "leg_controller/leg_controller.h"
 
-TEST(InverseDynamics, testTrue) {
+TEST(InverseDynamics, testConstructor) {
+  InverseDynamicsController inverse_dynamics_controller();
+  EXPECT_EQ(1 + 1, 2);
+}
+
+TEST(LegController, testConstructor) {
   ros::NodeHandle nh;
-  InverseDynamics inverse_dynamics(nh);
+  LegController leg_controller(nh);
   EXPECT_EQ(1 + 1, 2);
 }
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "inverse_dynamics_test");
+  ros::init(argc, argv, "leg_controller_test");
 
   return RUN_ALL_TESTS();
 }
