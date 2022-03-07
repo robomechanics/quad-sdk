9f1099cde2939256e6380fef52b7d19fa0953aec
Yanhao Yang <31764928+yanhaoy@users.noreply.github.com>
2022-03-07 09:28:27 -0500

    Improve swing foot plan (#214)
    * fix bugs, fix foothold heuristics, better swing foot acc limitation
    
    * remove online swing foot update, revert pd gains
    
    * Foothold nominal position minimize the maximum distance from hip plan
    MPC will not modify body and grf plan if fail to solve
    
    * fix centrifugal tracking term
    
    * Clean
    
    * Clean
    
    * Remove unused files and functions
    
    * Keep the original style
    
    * Fix some comments and warning info
    
    * Pass test
    
    * Resolve review requests
    
    * code format
    
    * Revert stand pose
    
    * Add getNextLiftoffIndex
    Use contact schedule for end_of_stance
    Move computeSwingApex to cpp
    
    * Fix test
    All the parameters should match the server...
    
    Co-authored-by: Joe Norby <jcnorby@gmail.com>

--- local_planner/test/test_local_planner.cpp
+++ local_planner/test/test_local_planner.cpp
@@ -20,8 +20,10 @@ TEST(LocalFootStepPlanner, baseCase) {
   double first_element_duration = dt;
   int period = 12;
   int num_feet = 4;
-  std::vector<double> duty_cycles = {0.3, 0.3, 0.3, 0.3};
-  std::vector<double> phase_offsets = {0.0, 0.5, 0.5, 0.0};
+  std::vector<double> duty_cycles;
+  ros::param::get("/local_footstep_planner/duty_cycles", duty_cycles);
+  std::vector<double> phase_offsets;
+  ros::param::get("/local_footstep_planner/phase_offsets", phase_offsets);
   double ground_clearance = 0.07;
   double hip_clearance = 0.1;
   double standing_error_threshold = 0.03;
@@ -29,7 +31,8 @@ TEST(LocalFootStepPlanner, baseCase) {
   double foothold_obj_threshold = 0.6;
   double grf_weight = 0.0;
   std::string obj_fun_layer = "traversability";
-  auto quadKD = std::make_shared<quad_utils::QuadKD>();
+  std::shared_ptr<quad_utils::QuadKD> quadKD =
+      std::make_shared<quad_utils::QuadKD>();
 
   LocalFootstepPlanner footstep_planner;
   footstep_planner.setTemporalParams(dt, period, N, duty_cycles, phase_offsets);
@@ -37,7 +40,7 @@ TEST(LocalFootStepPlanner, baseCase) {
       ground_clearance, hip_clearance, standing_error_threshold, grf_weight,
       quadKD, foothold_search_radius, foothold_obj_threshold, obj_fun_layer);
 
-  Eigen::MatrixXd body_plan(N, 12);
+  Eigen::MatrixXd body_plan(N + 1, 12);
   body_plan.fill(0);
   body_plan.col(2).fill(0.3);
 
@@ -57,7 +60,7 @@ TEST(LocalFootStepPlanner, baseCase) {
   }
 
   Eigen::MatrixXd grf_plan(N, 12);
-  grf_plan.fill(0.3);
+  grf_plan.fill(0);
 
   Eigen::MatrixXd foot_positions_world(N, 12);
   foot_positions_world.fill(0);
@@ -71,6 +74,7 @@ TEST(LocalFootStepPlanner, baseCase) {
   Eigen::VectorXd current_foot_positions_world(12);
   current_foot_positions_world << 0.2263, 0.17098, 0.02, -0.2263, 0.17098, 0.02,
       0.2263, -0.17098, 0.02, -0.2263, -0.17098, 0.02;
+  foot_positions_world.row(0) = current_foot_positions_world;
 
   Eigen::VectorXd current_foot_velocities_world(12);
   current_foot_velocities_world.fill(0);
