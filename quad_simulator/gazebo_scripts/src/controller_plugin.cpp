9ebba2c403acb15a6bd30ea5f72f6bfbdf38623d
ardalantj <ardalantj@yahoo.com>
2022-03-20 14:50:57 -0400

    multi robot sim working

--- quad_simulator/gazebo_scripts/src/controller_plugin.cpp
+++ quad_simulator/gazebo_scripts/src/controller_plugin.cpp
@@ -22,26 +22,26 @@ namespace effort_controllers {
  */
 SpiritController::SpiritController() {
   // Setup joint map
-  leg_map_[0] = std::make_pair(0, 1);   // hip0
-  leg_map_[1] = std::make_pair(0, 2);   // knee0
-  leg_map_[2] = std::make_pair(1, 1);   // hip1
-  leg_map_[3] = std::make_pair(1, 2);   // knee1
-  leg_map_[4] = std::make_pair(2, 1);   // hip2
-  leg_map_[5] = std::make_pair(2, 2);   // knee2
-  leg_map_[6] = std::make_pair(3, 1);   // hip3
-  leg_map_[7] = std::make_pair(3, 2);   // knee3
-  leg_map_[8] = std::make_pair(0, 0);   // abd0
-  leg_map_[9] = std::make_pair(1, 0);   // abd1
-  leg_map_[10] = std::make_pair(2, 0);  // abd2
-  leg_map_[11] = std::make_pair(3, 0);  // abd3
+  leg_map_[0] = std::make_pair(0, 1);  // hip0
+  leg_map_[1] = std::make_pair(0, 2);  // knee0
+  leg_map_[2] = std::make_pair(1, 1);  // hip1
+  leg_map_[3] = std::make_pair(1, 2);  // knee1
+  leg_map_[4] = std::make_pair(2, 1);  // hip2
+  leg_map_[5] = std::make_pair(2, 2);  // knee2
+  leg_map_[6] = std::make_pair(3, 1);  // hip3
+  leg_map_[7] = std::make_pair(3, 2);  // knee3
+  leg_map_[8] = std::make_pair(0, 0);  // abd0
+  leg_map_[9] = std::make_pair(1, 0);  // abd1
+  leg_map_[10] = std::make_pair(2, 0); // abd2
+  leg_map_[11] = std::make_pair(3, 0); // abd3
 
   // Torque saturation (could change to linear model in future)
   torque_lims_ = {21, 21, 32};
 }
 SpiritController::~SpiritController() { sub_command_.shutdown(); }
 
-bool SpiritController::init(hardware_interface::EffortJointInterface* hw,
-                            ros::NodeHandle& n) {
+bool SpiritController::init(hardware_interface::EffortJointInterface *hw,
+                            ros::NodeHandle &n) {
   // List of controlled joints
   std::string param_name = "joints";
   if (!n.getParam(param_name, joint_names_)) {
@@ -64,11 +64,11 @@ bool SpiritController::init(hardware_interface::EffortJointInterface* hw,
   }
 
   for (unsigned int i = 0; i < n_joints_; i++) {
-    const auto& joint_name = joint_names_[i];
+    const auto &joint_name = joint_names_[i];
 
     try {
       joints_.push_back(hw->getHandle(joint_name));
-    } catch (const hardware_interface::HardwareInterfaceException& e) {
+    } catch (const hardware_interface::HardwareInterfaceException &e) {
       ROS_ERROR_STREAM("Exception thrown: " << e.what());
       return false;
     }
@@ -88,14 +88,19 @@ bool SpiritController::init(hardware_interface::EffortJointInterface* hw,
   quad_utils::loadROSParam(n, "/topics/control/joint_command",
                            joint_command_topic);
 
+  auto ns = n.getNamespace();
+  std::string private_ns = "joint_controller";
+  ns.resize(ns.size() - private_ns.size());
+  joint_command_topic = "/" + ns + "/" + joint_command_topic;
+
   sub_command_ = n.subscribe<quad_msgs::LegCommandArray>(
       joint_command_topic, 1, &SpiritController::commandCB, this);
   return true;
 }
 
-void SpiritController::update(const ros::Time& time,
-                              const ros::Duration& period) {
-  BufferType& commands = *commands_buffer_.readFromRT();
+void SpiritController::update(const ros::Time &time,
+                              const ros::Duration &period) {
+  BufferType &commands = *commands_buffer_.readFromRT();
 
   // Check if message is populated
   if (commands.empty() || commands.front().motor_commands.empty()) {
@@ -143,24 +148,24 @@ void SpiritController::update(const ros::Time& time,
 }
 
 void SpiritController::commandCB(
-    const quad_msgs::LegCommandArrayConstPtr& msg) {
+    const quad_msgs::LegCommandArrayConstPtr &msg) {
   commands_buffer_.writeFromNonRT(msg->leg_commands);
 }
 
-void SpiritController::enforceJointLimits(double& command, unsigned int index) {
+void SpiritController::enforceJointLimits(double &command, unsigned int index) {
   // Check that this joint has applicable limits
   if (joint_urdfs_[index]->type == urdf::Joint::REVOLUTE ||
       joint_urdfs_[index]->type == urdf::Joint::PRISMATIC) {
-    if (command > joint_urdfs_[index]->limits->upper) {  // above upper limit
+    if (command > joint_urdfs_[index]->limits->upper) { // above upper limit
       command = joint_urdfs_[index]->limits->upper;
     } else if (command <
-               joint_urdfs_[index]->limits->lower) {  // below lower limit
+               joint_urdfs_[index]->limits->lower) { // below lower limit
       command = joint_urdfs_[index]->limits->lower;
     }
   }
 }
 
-}  // namespace effort_controllers
+} // namespace effort_controllers
 
 PLUGINLIB_EXPORT_CLASS(effort_controllers::SpiritController,
                        controller_interface::ControllerBase)
