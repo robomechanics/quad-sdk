fc85976e61adbc307e8b40c1d1c44642046edbbf
ardalantj <ardalantj@yahoo.com>
2022-03-28 18:45:32 -0400

    formatting

--- quad_utils/src/terrain_map_publisher.cpp
+++ quad_utils/src/terrain_map_publisher.cpp
@@ -108,8 +108,8 @@ void TerrainMapPublisher::updateMap() {
   }
 }
 
-std::vector<std::vector<double>>
-TerrainMapPublisher::loadCSV(std::string filename) {
+std::vector<std::vector<double>> TerrainMapPublisher::loadCSV(
+    std::string filename) {
   std::vector<std::vector<double>> data;
   std::ifstream inputFile(filename);
   int l = 0;
@@ -117,16 +117,14 @@ TerrainMapPublisher::loadCSV(std::string filename) {
   while (inputFile) {
     l++;
     std::string s;
-    if (!getline(inputFile, s))
-      break;
+    if (!getline(inputFile, s)) break;
     if (s[0] != '#') {
       std::istringstream ss(s);
       std::vector<double> record;
 
       while (ss) {
         std::string line;
-        if (!getline(ss, line, ','))
-          break;
+        if (!getline(ss, line, ',')) break;
         try {
           record.push_back(stod(line));
         } catch (const std::invalid_argument e) {
