74b96bf52b9c5380a3f2db9bfffac2c5dc10a272
Yanhao Yang <yanhaoy@andrew.cmu.edu>
2022-05-11 22:52:53 -0400

    clean local planner

--- local_planner/include/local_planner/local_planner.h
+++ local_planner/include/local_planner/local_planner.h
@@ -75,16 +75,10 @@ class LocalPlanner {
   void cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg);
 
   /**
-   * @brief Function to pre-process the body plan and robot state messages into
-   * Eigen arrays
+   * @brief Function to compute reference trajectory from twist command or
+   * global plan
    */
-  void getStateAndReferencePlan();
-
-  /**
-   * @brief Function to read robot state and twist command messages into Eigen
-   * arrays
-   */
-  void getStateAndTwistInput();
+  void getReference();
 
   /**
    * @brief Function to compute the local plan
@@ -166,12 +160,6 @@ class LocalPlanner {
   /// Current index in the global plan
   int current_plan_index_;
 
-  /// Minimum normal force in contact phase
-  double normal_lo_;
-
-  /// Maximum normal force in contact phase
-  double normal_hi_;
-
   /// local planner timestep (seconds)
   double dt_;
 
@@ -235,9 +223,6 @@ class LocalPlanner {
   /// Matrix of continuous foot positions in body frame
   Eigen::MatrixXd foot_positions_body_;
 
-  /// Matrix of continuous foot positions projected underneath the hips
-  Eigen::MatrixXd hip_projected_foot_positions_;
-
   /// Matrix of foot contact locations (number of contacts x num_legs_)
   Eigen::MatrixXd foot_plan_discrete_;
 
@@ -245,8 +230,7 @@ class LocalPlanner {
   std::shared_ptr<quad_utils::QuadKD> quadKD_;
 
   /// Twist input
-  typedef Eigen::VectorXd Twist;
-  Twist cmd_vel_;
+  Eigen::VectorXd cmd_vel_;
 
   /// Commanded velocity filter constant
   double cmd_vel_filter_const_;
@@ -255,7 +239,7 @@ class LocalPlanner {
   double cmd_vel_scale_;
 
   /// Nominal robot height
-  const double z_des_ = 0.27;
+  double z_des_;
 
   /// Time of the most recent cmd_vel data
   ros::Time last_cmd_vel_msg_time_;
@@ -282,7 +266,7 @@ class LocalPlanner {
   bool same_plan_index_;
 
   /// Toe radius
-  double toe_radius = 0.02;
+  double toe_radius_;
 
   /// Control mode
   int control_mode_;
