2e0d8a916aedf410195cec97f74b527dd6bf3033
jcnorby <jnorby@andrew.cmu.edu>
2022-06-10 15:32:16 -0400

    Squashed commit of the following:
    commit 1c05a0faa0f06be71220914555e7079aa5afb5d7
    Author: Alex Stutt <alexandra.stutt@gmail.com>
    Date:   Fri Jun 10 10:57:45 2022 -0700
    
        Minor documentation updates (#287)
    
        * removed old world file, added some pics for docs
    
        * Added Unitree A1 license.
    
        * pics of another world
    
        Co-authored-by: Justin Yim <jkyim@andrew.cmu.edu>
        Co-authored-by: Joe Norby <jnorby@andrew.cmu.edu>
    
    commit bae9ff0237784b692dbef41daa82a4e6c32688e3
    Merge: a0b9f9e3 7cdb9756
    Author: Bill Yu <60864752+qishuny@users.noreply.github.com>
    Date:   Mon Jun 6 08:55:03 2022 -0400
    
        Merge pull request #271 from robomechanics/melodic_devel_test_robot_driver
    
        Robot driver refactoring
    
    commit 7cdb975670af7e28726e63f2886af1f18491c5b3
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Fri Jun 3 18:48:57 2022 -0400
    
        additional changes to comments
    
    commit b577932ee5182376565512925aba127c02185053
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Fri Jun 3 18:47:13 2022 -0400
    
        Fixed params loading issue. Rewrite comments
    
    commit 4f83a42b41ec1079def70620524cfe1f1fdbff21
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Fri Jun 3 16:37:59 2022 -0400
    
        additional format fix
    
    commit 139550f125bb5b8e9ebcbca1479d33cbe24bce98
    Merge: cf2c9574 a0b9f9e3
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Fri Jun 3 11:08:04 2022 -0400
    
        Merge remote-tracking branch 'origin/devel' into melodic_devel_test_robot_driver
    
    commit cf2c9574a4cf338ac7b88fc9813d1bf34277df96
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Fri Jun 3 11:07:46 2022 -0400
    
        fix format
    
    commit a0b9f9e351f41812f1d4a4cd66b8fd47ab5ed83d
    Author: Joe Norby <jnorby@andrew.cmu.edu>
    Date:   Fri Jun 3 09:15:45 2022 -0400
    
        Update README.md
    
    commit b48c939243994d410968832b21e8944f62483262
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Thu Jun 2 11:12:42 2022 -0400
    
        Additional changes to estimator inheritance.
    
    commit 351de7d2974285971af6f07ffd65ab7e9e755feb
    Merge: 82c665d2 2e3962c7
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Wed May 25 14:54:56 2022 -0400
    
        Merge remote-tracking branch 'origin/devel' into melodic_devel_test_robot_driver
    
    commit 82c665d29176248d4d961aa4def17717ba97a273
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Tue May 24 21:26:47 2022 -0400
    
        change function input, fix lint
    
    commit 05a077a15491afae38a2508492cf75dc656805c5
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Tue May 24 00:26:32 2022 -0400
    
        in refactor branch move CF estimator from robot driver to its own class
    
    commit 81a5c9963b12db4b18e1152326eea3cbe3fc3c36
    Merge: 6fda82a0 88c4f1c3
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Mon May 23 23:37:42 2022 -0400
    
        Merge remote-tracking branch 'origin/devel' into melodic_devel_test_robot_driver
    
    commit 6fda82a0fd2f336ef95e907dfba5646027f54c0c
    Merge: 7fca4c1d a6e25dac
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Mon May 23 08:51:04 2022 -0400
    
        Merge remote-tracking branch 'origin/devel' into melodic_devel_test_robot_driver
    
    commit 7fca4c1dce53c426fd3ef4db677ce4888e1402ad
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Sat May 21 21:01:36 2022 -0400
    
        change cmake list format again
    
    commit 5d07d7c294bcd0b93de4d7e8efb554918a3bce9b
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Sat May 21 21:00:27 2022 -0400
    
        change robot driver cmake list format
    
    commit d9ff47dc9d60c4e1e1350931056edd8cc7780aa0
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Sat May 21 02:04:38 2022 -0400
    
        fix lint
    
    commit 8ff802c4a2cbe657bc5d4ae80d47b15dbd4c244e
    Author: qishuny <yuqishun1998@outlook.com>
    Date:   Sat May 21 01:52:36 2022 -0400
    
        added Ardalan's refactor to robot driver

---
+++ robot_driver/src/estimators/ekf_estimator.cpp
@@ -0,0 +1,12 @@
+#include "robot_driver/estimators/ekf_estimator.h"
+
+EKFEstimator::EKFEstimator() {}
+
+void EKFEstimator::init(ros::NodeHandle& nh) {
+  nh_ = nh;
+  std::cout << "EKF Estimator Initiated" << std::endl;
+}
+
+bool EKFEstimator::updateOnce(quad_msgs::RobotState& last_robot_state_msg) {
+  std::cout << "EKF Estimator Updated Once" << std::endl;
+}
