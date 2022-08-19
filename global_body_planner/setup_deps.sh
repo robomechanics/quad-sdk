d75c847274d9f68722f33fe1049c3b77d206fbec
Alex Stutt <alexandra.stutt@gmail.com>
2022-08-19 18:15:43 -0400

    switched installs from apt to rosdep in global_body_planner and quad_utils

--- global_body_planner/setup_deps.sh
+++ global_body_planner/setup_deps.sh
@@ -1,4 +1,6 @@
-sudo apt install -y ros-melodic-tf
-sudo apt install -y ros-melodic-tf2
-sudo apt install -y ros-melodic-tf2-geometry-msgs
-sudo apt install -y ros-melodic-grid-map
+#sudo apt install -y ros-melodic-tf
+#sudo apt install -y ros-melodic-tf2
+#sudo apt install -y ros-melodic-tf2-geometry-msgs
+#sudo apt install -y ros-melodic-grid-map
+
+# moving to package.xml using rosdep
