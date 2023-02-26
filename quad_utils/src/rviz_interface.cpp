d52db798eaf50bb0b800b73a84bc5b18ef828d17
dologan <ologandavid@gmail.com>
2023-02-26 14:39:48 -0800

    hardware changes

--- quad_utils/src/rviz_interface.cpp
+++ quad_utils/src/rviz_interface.cpp
@@ -557,7 +557,7 @@ void RVizInterface::robotStateCallback(
   } else if (pub_id == GROUND_TRUTH) {
     transformStamped.child_frame_id = tf_prefix_ + "_ground_truth/body";
 
-    ground_truth_base_tf_br_.sendTransform(transformStamped);
+    //ground_truth_base_tf_br_.sendTransform(transformStamped);
     ground_truth_joint_states_viz_pub_.publish(joint_msg);
 
     if (ground_truth_state_trace_msg_.points.empty()) {
