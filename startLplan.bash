cf571f1ad7fac9d55357cdb6c7cd3a8efe151d53
NikhilCG26 <nikhilcg26@gmail.com>
2023-07-10 16:55:28 -0400

    First Commit

---
+++ startLplan.bash
@@ -0,0 +1,12 @@
+#!/bin/bash         
+
+
+echo "set up Sims"
+
+cd ~/catkin_ws3
+source devel/setup.bash
+
+echo "done!"
+
+rostopic pub /robot_1/control/mode std_msgs/UInt8 "data: 1"
+roslaunch quad_utils quad_plan.launch reference:=twist twist_input:=keyboard
---
+++ startsim.bash
@@ -0,0 +1,12 @@
+#!/bin/bash         
+
+
+echo "set up Sims"
+
+cd ~/catkin_ws3
+source devel/setup.bash
+
+echo "done!"
+
+# roslaunch quad_utils quad_gazebo.launch gui:=true rviz_gui:=true
+roslaunch quad_utils quad_gazebo.launch gui:=true rviz_gui:=true robot_type:=test_cg
