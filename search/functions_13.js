f3fa976a7c37209ee0e7ad5f9a7ff8063b248b0b
ardalantj <ardalantj@users.noreply.github.com>
2023-10-13 15:43:05 +0000

    [ci skip] Feature/fix yaw wrapping (#400)
    * Fix yaw wrapping issue by expanding bounds and unwrapping reference
    
    * Remove duplicated unwrapping code
    
    * Linting Update
    
    * minor fix
    
    ---------
    
    Co-authored-by: dologan <ologandavid@gmail.com>
    Co-authored-by: Ardalan Tajbakhsh <ardalantajbakhsh@ardalans-mbp.wifi.local.cmu.edu> 640308511fe824cb8f13ec1750f402a1fa521de7

--- search/functions_13.js
+++ search/functions_13.js
@@ -1,16 +1,17 @@
 var searchData=
 [
   ['unloadinterface_0',['unloadInterface',['../classSpiritInterface.html#acb8c0ec8321cd14a6cdc3371666d1092',1,'SpiritInterface::unloadInterface()'],['../classHardwareInterface.html#a5f807b2896484f6dc4c0bae53102c007',1,'HardwareInterface::unloadInterface()']]],
-  ['updateadaptivecomplexityschedule_1',['updateAdaptiveComplexitySchedule',['../classNMPCController.html#aa9e6d32416ab5bf01bc4c1d68659312b',1,'NMPCController']]],
-  ['updatecontinuousplan_2',['updateContinuousPlan',['../classLocalFootstepPlanner.html#aba0d461220650f7f0ebbd484359df2e4',1,'LocalFootstepPlanner']]],
-  ['updatecontrol_3',['updateControl',['../classRobotDriver.html#a449265aa28c4c3741fd5df6e6e485a29',1,'RobotDriver']]],
-  ['updategvalue_4',['updateGValue',['../classGraphClass.html#ae87d58394dc58f56bd4f619193423046',1,'GraphClass']]],
-  ['updatehorizonlength_5',['updateHorizonLength',['../classNMPCController.html#a174edff8e1e5a9e0be8df96de7590b2b',1,'NMPCController']]],
-  ['updatelocalplanmsg_6',['updateLocalPlanMsg',['../classLegController.html#afe3a543a56691536aa58902ad8ca0008',1,'LegController']]],
-  ['updatemap_7',['updateMap',['../classLocalFootstepPlanner.html#a01dd97f2dce74cc1461dc0192b08949f',1,'LocalFootstepPlanner::updateMap()'],['../classTerrainMapPublisher.html#a23ff09b7adb01ba0a256c4ae5eb5865f',1,'TerrainMapPublisher::updateMap()'],['../classLocalFootstepPlanner.html#ac6e2f701d80dcf807582e84478a49b35',1,'LocalFootstepPlanner::updateMap()']]],
-  ['updateonce_8',['updateOnce',['../classCompFilterEstimator.html#a9fe49ef8b982023da5a168f12dbdb1e3',1,'CompFilterEstimator::updateOnce()'],['../classEKFEstimator.html#a92983c15b159bd5e1fed3a7cf0df1e09',1,'EKFEstimator::updateOnce()'],['../classStateEstimator.html#a60c53e404013cf7f2fc434d08273971c',1,'StateEstimator::updateOnce()']]],
-  ['updateparams_9',['updateParams',['../classTerrainMapPublisher.html#ac0e9d8d230df9fe17208038f7edc3677',1,'TerrainMapPublisher']]],
-  ['updatestate_10',['updateState',['../classRobotDriver.html#a0b2a5da3c02a06beadf110c44f91fdeb',1,'RobotDriver']]],
-  ['updateterrainheight_11',['updateTerrainHeight',['../classGlobalBodyPlannerTestFixture.html#aaccca983bcfb29f916b835426217def1',1,'GlobalBodyPlannerTestFixture']]],
-  ['updateterrainslope_12',['updateTerrainSlope',['../classGlobalBodyPlannerTestFixture.html#a799d7684f11ecc501b8b2a369f197aaf',1,'GlobalBodyPlannerTestFixture']]]
+  ['unwrapyawreference_1',['unwrapYawReference',['../classLocalPlanner.html#ac31f4372e49f80b3ad7400569d05d3ae',1,'LocalPlanner']]],
+  ['updateadaptivecomplexityschedule_2',['updateAdaptiveComplexitySchedule',['../classNMPCController.html#aa9e6d32416ab5bf01bc4c1d68659312b',1,'NMPCController']]],
+  ['updatecontinuousplan_3',['updateContinuousPlan',['../classLocalFootstepPlanner.html#aba0d461220650f7f0ebbd484359df2e4',1,'LocalFootstepPlanner']]],
+  ['updatecontrol_4',['updateControl',['../classRobotDriver.html#a449265aa28c4c3741fd5df6e6e485a29',1,'RobotDriver']]],
+  ['updategvalue_5',['updateGValue',['../classGraphClass.html#ae87d58394dc58f56bd4f619193423046',1,'GraphClass']]],
+  ['updatehorizonlength_6',['updateHorizonLength',['../classNMPCController.html#a174edff8e1e5a9e0be8df96de7590b2b',1,'NMPCController']]],
+  ['updatelocalplanmsg_7',['updateLocalPlanMsg',['../classLegController.html#afe3a543a56691536aa58902ad8ca0008',1,'LegController']]],
+  ['updatemap_8',['updateMap',['../classLocalFootstepPlanner.html#ac6e2f701d80dcf807582e84478a49b35',1,'LocalFootstepPlanner::updateMap()'],['../classTerrainMapPublisher.html#a23ff09b7adb01ba0a256c4ae5eb5865f',1,'TerrainMapPublisher::updateMap()'],['../classLocalFootstepPlanner.html#a01dd97f2dce74cc1461dc0192b08949f',1,'LocalFootstepPlanner::updateMap()']]],
+  ['updateonce_9',['updateOnce',['../classCompFilterEstimator.html#a9fe49ef8b982023da5a168f12dbdb1e3',1,'CompFilterEstimator::updateOnce()'],['../classEKFEstimator.html#a92983c15b159bd5e1fed3a7cf0df1e09',1,'EKFEstimator::updateOnce()'],['../classStateEstimator.html#a60c53e404013cf7f2fc434d08273971c',1,'StateEstimator::updateOnce()']]],
+  ['updateparams_10',['updateParams',['../classTerrainMapPublisher.html#ac0e9d8d230df9fe17208038f7edc3677',1,'TerrainMapPublisher']]],
+  ['updatestate_11',['updateState',['../classRobotDriver.html#a0b2a5da3c02a06beadf110c44f91fdeb',1,'RobotDriver']]],
+  ['updateterrainheight_12',['updateTerrainHeight',['../classGlobalBodyPlannerTestFixture.html#aaccca983bcfb29f916b835426217def1',1,'GlobalBodyPlannerTestFixture']]],
+  ['updateterrainslope_13',['updateTerrainSlope',['../classGlobalBodyPlannerTestFixture.html#a799d7684f11ecc501b8b2a369f197aaf',1,'GlobalBodyPlannerTestFixture']]]
 ];
