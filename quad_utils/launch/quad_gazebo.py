fb7d4433e10321d8d5ed0c0a3de78111bcdac0ca
ologandavid <ologandavid@gmail.com>
2025-06-26 17:30:57 -0400

    Before Editing Launch Sequence

--- quad_utils/launch/quad_gazebo.py
+++ quad_utils/launch/quad_gazebo.py
@@ -136,7 +136,7 @@ def generate_launch_description():
         DeclareLaunchArgument('use_sim_time', default_value='true', description='Whether to use Computer Clock or Sim Clock'),
         DeclareLaunchArgument(
             'robot_configs',
-            default_value='[{"name": "robot_1", "type": "go2", "controller": "inverse_dynamics"}]',
+            default_value='[{"name": "robot_1", "type": "spirit", "controller": "inverse_dynamics"}]',
             description='A JSON List of robot configurations: MUST specifiy name, type, and controller'
         ),
     ]
