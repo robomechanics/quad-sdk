fc4a48c69bb7dc190120bd529ed14f9566c054a4
David Ologan <ologandavid@gmail.com>
2026-03-18 13:08:32 -0400

    Modified Automatic Testing Script

--- quad_simulator/setup_deps.sh
+++ quad_simulator/setup_deps.sh
@@ -29,11 +29,11 @@ else
 fi
 
 sudo apt-get update
-sudo apt-get install lsb-release gnupg
+sudo apt-get install -y lsb-release gnupg
 sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 sudo apt-get update
-sudo apt-get install gz-harmonic
+sudo apt-get install -y gz-harmonic
 sudo apt install -y mesa-utils\
       ros-jazzy-gz-tools-vendor\
       ros-jazzy-gz-sim-vendor
