2a074df3230daecd989f09742c841833862636d1
David Ologan <ologandavid@gmail.com>
2026-03-24 22:26:01 -0400

    Created Hardware Branch, Generated a Simple Jazzy Dockerfile for Hardware Deployment

--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# Author : Taeyoung Kim (https://github.com/Taeyoung96)
 
-# Set the project directory (PROJECT_DIR) as the parent directory of the current working directory
 PROJECT_DIR=$(dirname "$PWD")
-
-# Move to the parent folder of the project directory
 cd "$PROJECT_DIR"
 
-# Print the current working directory to verify the change
-echo "Current working directory: $PROJECT_DIR"
-
-# Check if arguments are provided for the image name and tag
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <container_name> <image_name:tag>"
   exit 1
@@ -19,9 +11,6 @@ CONTAINER_NAME="$1"
 IMAGE_NAME="$2"
 
 docker run --privileged -it \
-            --gpus='all,"capabilities=compute,utility,graphics,display"' \
-           -e NVIDIA_DRIVER_CAPABILITIES=all \
-           -e NVIDIA_VISIBLE_DEVICES=all \
            --volume="$PROJECT_DIR:/root/ros2_ws/src/quad-sdk" \
            --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
            --net=host \
@@ -29,5 +18,7 @@ docker run --privileged -it \
            --shm-size=4gb \
            --name="$CONTAINER_NAME" \
            --env="DISPLAY=$DISPLAY" \
+           --env="ROS_DOMAIN_ID=42" \
+           --env="ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET" \
            --rm \
            "$IMAGE_NAME" /bin/bash
