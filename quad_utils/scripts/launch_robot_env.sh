655f710859b5d8ec121b197c0dfa22d718206115
thomaslee6710 <prothomascflee@gmail.com>
2026-05-26 11:31:06 -0400

    add learned go2 hardware support

--- quad_utils/scripts/launch_robot_env.sh
+++ quad_utils/scripts/launch_robot_env.sh
@@ -19,8 +19,8 @@ chronyc sources || true
 echo "Sourcing quad_utils/scripts/init_robot.sh to source env and setup IPs"
 source ~/ros2_ws/src/quad-sdk/quad_utils/scripts/init_robot.sh
 
-echo "Launch robot_driver.launch with inverse dynamics controller? (y/n)"
+echo "Launch robot_driver.py with inverse dynamics controller? (y/n)"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
-	ros2 launch quad_utils robot_driver.launch mocap:=true hardware:=true
+	ros2 launch quad_utils robot_driver.py mocap:=true is_hardware:=true
 fi
