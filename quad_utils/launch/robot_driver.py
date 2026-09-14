0e44351ec765ea7b7622e98c04873cd4a6792756
thomaslee6710 <prothomascflee@gmail.com>
2026-09-14 18:12:23 -0400

    zero shot transfer, gotta rmb the gains

--- quad_utils/launch/robot_driver.py
+++ quad_utils/launch/robot_driver.py
@@ -51,7 +51,7 @@ def generate_launch_description():
     robot_type = DeclareLaunchArgument('robot_type', default_value='go2')
     mocap = DeclareLaunchArgument('mocap', default_value='true')
     logging = DeclareLaunchArgument('logging', default_value='false')
-    controller = DeclareLaunchArgument('controller', default_value='inverse_dynamics')
+    controller = DeclareLaunchArgument('controller', default_value='underbrush_v90')
     model_path = DeclareLaunchArgument('model_path', default_value='',
         description='Optional ONNX path; empty uses robot_driver.yaml')
     provider = DeclareLaunchArgument('provider', default_value = "cpu")
